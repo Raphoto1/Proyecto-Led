ebd7becac060ec17863eff9c9ed5f25ec1d92c16
Rafael Martinez <rrhhmmtt@gmail.com>
2022-08-19 18:24:09 -0500

    Primer Commit

---
+++ led.js
@@ -0,0 +1,7 @@
+//Trabajar con 
+const tModulo = 50;
+let pixeles;
+let tTotalX;
+let tTotalY;
+let tPixeles;
+//calcular cantidad de pixeles por el pitch(densidad de pixeles) con tamaño de modulo constante
