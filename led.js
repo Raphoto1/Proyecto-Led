aa28e2d0c419078ccd9529160128a81745c7756a
Rafael Martinez <rrhhmmtt@gmail.com>
2022-08-19 18:42:00 -0500

    Planteamiento de Variables 1

--- led.js
+++ led.js
@@ -1,7 +1,20 @@
-//Trabajar con 
+//Trabajar con tamaño de modulo constante
 const tModulo = 50;
-let pixeles;
+//variable para pitch
+let pitch;
+//pixeles por modulo X y Y
+let mPixeles;
+//tamaño total de la pantalla en X en cm
 let tTotalX;
+//tamaño total de la pantalla en Y en cm
 let tTotalY;
-let tPixeles;
+//cantidad de pixeles en total
+let tPixelesT;
+//cantidad de pixeles en x
+let tPixelesX;
+//cantidad de pixeles en y
+let tPixelesY;
 //calcular cantidad de pixeles por el pitch(densidad de pixeles) con tamaño de modulo constante
+//calcular el total de pixeles en X
+//calcular el total de pixeles en Y
+//calcular el total de pixeles en Total
